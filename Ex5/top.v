49d42e44a2400e31b99c3d513372fbdc794f7a98
Cloud User <centos@ip-172-31-35-119.eu-west-2.compute.internal>
2021-05-26 15:46:53 +0000

    Ex5 solution

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,48 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module air_cond (
+	input clk,
+	input [4:0]temperature,
+	output reg heating,
+	output reg cooling
+
+		);
+
+always @(posedge clk) begin
+
+if(temperature>=22)
+	begin
+	cooling=1;
+	heating=0;
+	end
+else if(temperature<=18)
+	begin
+	cooling=0;
+	heating=1;
+	end
+else if((temperature>20)&&(heating==5'b1))
+	begin
+	heating=1;
+	cooling=0;
+	end
+else if((temperature<=20)&&(heating==5'b1))
+	begin
+	heating=0;
+	cooling=0;
+	end
+else if((temperature<20)&&(cooling==5'b1))
+	begin
+	cooling=0;
+	heating=1;
+	end
+else if((temperature>=20)&&(cooling==5'b1))
+	begin
+	cooling=0;
+	heating=0;
+	end
+
+end
+endmodule
